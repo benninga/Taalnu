388fb9d0caa01a2f6e0da334953cf39f3963c837
Amos Benninga <amos@taalnu.com>
2012-09-04 22:00:02 -0400

    First commit. Skeleton Ruby on Rails.
    Empty rails application with initial work to connect to Mongo DB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taalnu::Application.config.secret_token = '5eae9e3113bf510bc7b51aa1a8ab55a4d0843e21fd05e600d7520fbe302eb7058c436d0c7de6a419b86bbd9f932a6b471d1ccaece704a823411304b5966073cd'
